83f428ae92a80f13c32b839a27c5809d1647ba5e
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-26 22:23:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    temp = array.last(2).join(", and ")
+    array.first(array.length-2).join(", ") + ", " + temp
+    
 
+  end
 end
