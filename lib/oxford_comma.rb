62231c51945702570b5d5a97f89612253235c7ab
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-26 22:05:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,10 @@
 def oxford_comma(array)
   if array.length == 1
     return "#{array[0]}"
-elsif array.length == 2
-return array.join(" and ")
-elsif array.length >= 3
-return array.join("," " and ")
-end
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+  
+    return array.join("," " and ")
+  end
 end
